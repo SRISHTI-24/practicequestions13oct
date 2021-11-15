beebb66bb2b0cfb0d93ce994973b67c3c3e2930c
SRISHTI-24 <srishtimsaxena2002@gmail.com>
2021-11-15 23:58:24 +0530

    interface questions

--- src/library.java
+++ src/library.java
@@ -1,37 +1,60 @@
 interface libraryuser {
-    public abstract void registeraccount();
-    public abstract void requestbook();
+    public abstract void registeraccount(int age);
+    public abstract void requestbook(String booktype);
 }
 class kidUsers implements libraryuser
 {
-    public void registeraccount()
+    int age;
+    String booktype;
+    public void registeraccount(int age)
     {
-        System.out.println("kidusers register account");
+        if(age<12)
+        {
+            System.out.println("you have successfully registered");
+        }
+        else if(age>12)
+        {
+            System.out.println("Sorry, age must be less than 12 to register as a kid");
+        }
     }
-    public void requestbook()
+    public void requestbook(String booktype)
     {
-        System.out.println("kidusers requestbook");
+        if(booktype=="Kids")
+            System.out.println("book issued successfully, please return the book within 10 days");
+        else
+            System.out.println("Oops, you are allowed to take only lids books");
+
     }
 }
 class Adultuser implements libraryuser
 {
-    public void registeraccount()
+    public void registeraccount(int age)
     {
-        System.out.println("Adultuser register account");
+        if(age>12)
+            System.out.println("you have successfully registered");
+        else if(age<12)
+            System.out.println("Sorry, age is must be graeter than 12 to register as an adult user");
     }
-    public void requestbook()
+    public void requestbook(String booktype)
     {
-        System.out.println("Adultuser requestbook");
+        if(booktype=="Fiction")
+            System.out.println("Book Issued successfully, please return book within 7 days");
+        else
+            System.out.println("Oops, you are allowed to take only adult Fiction books");
     }
 }
-class library
+class libraryinterfaceDemo
 {
     public static void main(String[] args) {
         kidUsers l = new kidUsers();
-        l.registeraccount();
-        l.requestbook();
+        l.registeraccount(10);
+        l.registeraccount(18);
+        l.requestbook("Kids");
+        l.requestbook("Fiction");
         Adultuser d = new Adultuser();
-        d.registeraccount();
-        d.requestbook();
+        d.registeraccount(5);
+        d.registeraccount(23);
+        d.requestbook("Fiction");
+        d.requestbook("Kids");
     }
 }
